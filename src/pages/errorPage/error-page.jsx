11bf016a48afcc97d38be129664a5b0d04a28774
kate4921 <peslyakkate@gmail.com>
2023-05-09 21:25:26 +0300

    errorpage

--- src/pages/errorPage/error-page.jsx
+++ src/pages/errorPage/error-page.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
 import './errorpage.css'
 
 export default function ErrorPage() {
@@ -13,6 +13,7 @@ export default function ErrorPage() {
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
+        <Link className='errorlink' to='/recipes'>Go back to choosing recipes</Link>
       </div>
     </div>
   )
